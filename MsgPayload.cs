96935a0197f31b6e7dd5cd698a71beb930c9e7bb
Tushar <53821502+TheTusharS@users.noreply.github.com>
2019-08-12 11:48:51 +0530

    Initial Commit

---
+++ MsgPayload.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scratch
+{
+    class MsgPayload
+    {
+        public string text { get; set; }
+        public string channel { get; set; }
+    }
+}
---
+++ PostMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace Scratch
+{
+    class PostMessage
+    {
+        private readonly RestClient client;
+        private readonly RestRequest request;
+        private readonly string TOKEN;
+        public PostMessage(string token)
+        {
+            TOKEN = token;
+            client = new RestClient("https://slack.com/api/chat.postMessage");
+            request = new RestRequest(Method.POST);
+        }
+        public string GetRestResponse(MsgPayload msgPayload)
+        {
+            string JsonMessage = JsonConvert.SerializeObject(msgPayload);
+            request.AddHeader("Authorization", "Bearer " + TOKEN);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("undefined", JsonMessage, ParameterType.RequestBody);
+            return client.Execute(request).Content;
+        }
+    }
+}
